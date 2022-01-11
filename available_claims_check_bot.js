9e76dfaadae638beddbe822e749bbdc6ef6d2ab7
nikharsha <nik.harsha@gmail.com>
2022-01-11 01:11:06 -0700

    Update available_claims_check_bot.js

--- available_claims_check_bot.js
+++ available_claims_check_bot.js
@@ -6,7 +6,15 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 filePath = './snapshot.json';
-var data = JSON.parse(fs.readFileSync(filePath));
+var raw = JSON.parse(fs.readFileSync(filePath));
+
+var key, keys = Object.keys(raw);
+var n = keys.length;
+var data={}
+while (n--) {
+  key = keys[n];
+  data[key.toLowerCase()] = raw[key];
+}
 
 client.on('message', async message => {
 
@@ -21,10 +29,10 @@ client.on('message', async message => {
 
   let wallet = args[0].toLowerCase();
 
-  msg = 'Wallet ' + wallet + ' **DOES NOT HOLD** enough Bananas to claim a FREE Hand! :(';
+  msg = 'Wallet' + wallet + ' **DOES NOT HOLD** enough Bananas to claim a FREE Hand! :(';
 
-  if (wallet in data.toLowerCase()) {
-    msg = 'Wallet ' + wallet + ' **CAN CLAIM ' + data[wallet] + ' ** FREE Hands, Yay! :)';
+  if (wallet in data) {
+    msg = 'Wallet' + wallet + ' **CAN CLAIM ' + data[wallet] + ' ** FREE Hands, Yay! :)';
   }
 
   message.channel.send(msg); 
